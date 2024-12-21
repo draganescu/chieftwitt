198bfa643ea3c7158d631148ac65b21adcdf809a
Andrei Draganescu <andrei.draganescu@automattic.com>
2024-12-21 18:34:48 +0200

    makes the client a PWA and remove cert req for building

--- chieftwitt-client/vite.config.ts
+++ chieftwitt-client/vite.config.ts
@@ -3,17 +3,26 @@ import react from '@vitejs/plugin-react'
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
 import { fileURLToPath } from 'url'
+import type { ServerOptions } from 'vite'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
+export default defineConfig(({ command }) => {
+  const serverConfig: ServerOptions = {
+    host: 'localhost',
+  }
+
+  // Only add HTTPS configuration in development mode
+  if (command === 'serve') {
+    serverConfig.https = {
       key: readFileSync(resolve(__dirname, './.cert/key.pem')),
       cert: readFileSync(resolve(__dirname, './.cert/cert.pem')),
-    },
-    host: 'localhost',
-  },
+    }
+  }
+
+  return {
+    plugins: [react()],
+    server: serverConfig
+  }
 })
